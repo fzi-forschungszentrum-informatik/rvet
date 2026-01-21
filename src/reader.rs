46f5b21acd6a0787206dd7f1748d07eaeaed82d9
Julian Ganz <ganz@fzi.de>
2026-01-21 15:39:47 +0100

    reader: add a BufReader reimplementation with peek fn
    We'll need to read traces from a file sooner or later. Ideally, we want
    to load data as needed. The decoder provided by `riscv_etrace` requires
    encoded packet data to reside in contiguous memory, and buffering reads
    is something we should do anyway.
    
    Using a `std::io::BufReader` would be the natural choice, but coping
    with packets spanning reads means we either need an additional buffer or
    means to force a `BufRead` to refill its buffer even though there is
    still data left unconsumed. The latter _is_ possible via the unstable
    `peek` (feature `bufreader_peek`).
    
    Since we do not want to depend on unstable code, we decided to introduce
    our own hacket-together `BufReader` which implementes just the fns that
    we need (and nothing else) in the hope that `bufreader_peek` will be
    stabilized at some point. If/when that happens, we can just throw this
    out and use `std::io::BufReader`.

--- src/reader.rs
+++ src/reader.rs
@@ -35,3 +35,59 @@ impl PacketHandler for DefaultHandler {
         Ok(None)
     }
 }
+
+/// Partial reimpl of [`std::io::BufReader`] with associated `peek` fn
+struct BufReader<R: std::io::Read> {
+    inner: R,
+    buffer: Vec<std::mem::MaybeUninit<u8>>,
+    pos: usize,
+}
+
+impl<R: std::io::Read> BufReader<R> {
+    /// Create a new reader
+    pub fn new(inner: R) -> Self {
+        let buffer = Box::new_uninit_slice(16 * 1024).into();
+        Self {
+            inner,
+            buffer,
+            pos: usize::MAX,
+        }
+    }
+
+    /// Retrieve some number of bytes without advancing the read position
+    pub fn peek(&mut self, n: usize) -> std::io::Result<&[u8]> {
+        if self.pos.saturating_add(n) > self.buffer.len() {
+            let len = self.buffer.len();
+            self.buffer.copy_within(self.pos..len, 0);
+            let read_base = len - self.pos;
+            self.pos = 0;
+
+            let (_, availible) = self.buffer.split_at_mut(read_base);
+            let read_len = self.inner.read(unsafe { availible.assume_init_mut() })?;
+
+            self.buffer.truncate(read_base.saturating_add(read_len));
+        }
+
+        let availible = &self.buffer[self.pos..];
+        let res = &availible[..n];
+        Ok(unsafe { res.assume_init_ref() })
+    }
+
+    /// Fill the internal buffer if we ran out of data
+    fn fill_buf(&mut self) -> std::io::Result<&[u8]> {
+        if self.pos >= self.buffer.len() {
+            let buf = self.buffer.as_mut_slice();
+            let read_len = self.inner.read(unsafe { buf.assume_init_mut() })?;
+            self.pos = 0;
+            self.buffer.truncate(read_len);
+        }
+
+        let res = &self.buffer[self.pos..];
+        Ok(unsafe { res.assume_init_ref() })
+    }
+
+    /// Consume the given number of bytes
+    pub fn consume(&mut self, amount: usize) {
+        self.pos = self.pos.saturating_add(amount);
+    }
+}

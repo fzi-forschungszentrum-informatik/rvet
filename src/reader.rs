b864d7ce2f4a87fa2b0d243c1a37ceb279fd2fec
Julian Ganz <ganz@fzi.de>
2026-01-21 16:35:27 +0100

    reader: add Reader for individual packets
    We'll need to read traces from a file sooner or later. Ideally, we want
    to load data as needed.
    
    The `riscv_etrace` decoder's requirement of packet data to residing in
    contiguous memory makes the buffer management a bit more involved. We
    therefore decided to abstract this logic away in a `Reader`.

--- src/reader.rs
+++ src/reader.rs
@@ -2,10 +2,112 @@
 // SPDX-License-Identifier: Apache-2.0
 //! Packet/payload reading and decoding utilities
 
+use std::fs::File;
+
+use anyhow::Context;
 use riscv_etrace::packet;
 
 use packet::decoder::Decoder;
 use packet::unit::Plug;
+
+/// Reader/processor for packets
+///
+/// Instances read and processe packets using a [`PacketHandler`]. It functions
+/// as an [`Iterator`] yielding any handler result that is not [`None`].
+pub struct Reader<H: PacketHandler = DefaultHandler> {
+    reader: BufReader<File>,
+    builder: packet::Builder<Plug>,
+    handler: H,
+}
+
+impl Reader {
+    /// Create a new reader
+    ///
+    /// Attempts to open the trace file at `trace_file`. On success a reader is
+    /// constructed with the opened file, `builder` and a default handler.
+    pub fn new(
+        trace_file: &std::path::Path,
+        builder: packet::Builder<Plug>,
+    ) -> anyhow::Result<Self> {
+        File::open(trace_file)
+            .map(|f| Self {
+                reader: BufReader::new(f),
+                builder,
+                handler: Default::default(),
+            })
+            .with_context(|| format!("Could not open trace file {}", trace_file.display()))
+    }
+}
+
+impl<H: PacketHandler> Reader<H> {
+    /// Replaces the handler of this reader
+    pub fn with_handler<N: PacketHandler>(self, handler: N) -> Reader<N> {
+        Reader {
+            reader: self.reader,
+            builder: self.builder,
+            handler,
+        }
+    }
+
+    /// Read/process a single packet
+    ///
+    /// Calls [`PacketHandler::handle`] with a new [`Decoder`]. If the result
+    /// indicates [insufficient data][packet::error::Error::InsufficientData],
+    /// the process is repeated with a larger buffer if possible.
+    ///
+    /// # Note
+    ///
+    /// Users are expected to use the [`Iterator`] interface instead if
+    /// applicable.
+    pub fn read_packet(&mut self) -> anyhow::Result<Option<H::Output>> {
+        loop {
+            let buf = self
+                .reader
+                .fill_buf()
+                .context("Could not read from trace file")?;
+            if buf.is_empty() {
+                return Ok(None);
+            }
+
+            let mut decoder = self.builder.decoder(buf);
+            match self.handler.handle(&mut decoder) {
+                Ok(r) => {
+                    let read = buf.len() - decoder.bytes_left();
+                    self.reader.consume(read);
+                    if let Some(res) = r {
+                        return Ok(Some(res));
+                    }
+                }
+                Err(e) => {
+                    if let Some(packet::error::Error::InsufficientData(need)) = e.downcast_ref() {
+                        // The buffer did not contain a full packet, so we try
+                        // to load at least the amount of data needed to carry
+                        // on.
+                        let need = buf.len().saturating_add(need.get());
+
+                        let buf = self
+                            .reader
+                            .peek(need)
+                            .context("Could not read from trace file")?;
+                        if buf.len() < need {
+                            anyhow::bail!("Reached end of file while decoding a packet");
+                        }
+                    } else {
+                        return Err(e);
+                    }
+                }
+            };
+        }
+    }
+}
+
+impl<H: PacketHandler> Iterator for Reader<H> {
+    type Item = anyhow::Result<H::Output>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.read_packet().transpose()
+    }
+}
 
 /// Handler for packets
 pub trait PacketHandler {

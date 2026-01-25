d358fa6d5e8d693fb7d5ebecaa8e7189eee5c3be
Julian Ganz <ganz@fzi.de>
2026-01-25 13:44:02 +0100

    reader: add module for packet reading utilities
    We'll need to read traces from a file sooner or later. Ideally, we want
    to load data as needed. As this will be more involved, we decided to
    introduce a few utilities that we can use in different subcommands in
    the future. This change introduces a module for those utilities.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod cli;
+mod reader;
 
 fn main() -> anyhow::Result<()> {
     let args: cli::Cli = clap::Parser::parse();
---
+++ src/reader.rs
@@ -0,0 +1,3 @@
+// Copyright (C) 2026 FZI Forschungszentrum Informatik
+// SPDX-License-Identifier: Apache-2.0
+//! Packet/payload reading and decoding utilities

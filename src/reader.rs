5eb85d40cf6f35ca9620c91bd1cc695d6d2b1ebe
Julian Ganz <ganz@fzi.de>
2026-01-25 13:57:04 +0100

    reader: add trait abstracting handlers for packets
    We'll need to read traces from a file sooner or later. Ideally, we want
    to load data as needed. As this will be more involved, we decided to
    introduce a few utilities that we can use in different subcommands in
    the future. The handling of packets will, however, differ between
    subcommands.
    
    Ideally, we would just want an `Iterator` over packets that we then
    process however we want. Still, we'll need to defer decoding payloads to
    cope with packets not containing trace payloads that may show up in a
    trace file. Combined with lifetime constraints this obstructs the
    classical `Iterator` appraoch. Worse yet, we need to bundle the actual
    packet decoding to avoid messy reading logic later on.
    
    All this lead us to introduce, with this change, a trait that abstracts
    decoding and handling a packet (i.e. decide whether or not to decode its
    payload), at least to a point where we have some `'static' entity to
    work with.

--- src/reader.rs
+++ src/reader.rs
@@ -1,3 +1,37 @@
 // Copyright (C) 2026 FZI Forschungszentrum Informatik
 // SPDX-License-Identifier: Apache-2.0
 //! Packet/payload reading and decoding utilities
+
+use riscv_etrace::packet;
+
+use packet::decoder::Decoder;
+use packet::unit::Plug;
+
+/// Handler for packets
+pub trait PacketHandler {
+    /// Type of output resulting from processing a single packet
+    type Output;
+
+    /// Decode and handle a single packet
+    fn handle(&mut self, decoder: &mut Decoder<'_, Plug>) -> anyhow::Result<Option<Self::Output>>;
+}
+
+impl<T: PacketHandler> PacketHandler for &mut T {
+    type Output = T::Output;
+
+    fn handle(&mut self, decoder: &mut Decoder<'_, Plug>) -> anyhow::Result<Option<Self::Output>> {
+        T::handle(self, decoder)
+    }
+}
+
+/// A dummy [`PacketHandler`]
+#[derive(Copy, Clone, Default, Debug)]
+pub struct DefaultHandler;
+
+impl PacketHandler for DefaultHandler {
+    type Output = ();
+
+    fn handle(&mut self, _decoder: &mut Decoder<'_, Plug>) -> anyhow::Result<Option<Self::Output>> {
+        Ok(None)
+    }
+}

4ea55fe2d468ea733b9fefac854d97f9cc155c52
Julian Ganz <ganz@fzi.de>
2026-01-27 09:31:57 +0100

    main: add CLI option for the packet format
    We support multiple packet formats. We want to allow selection via a CLI
    option and we want to parse that as an enum.

--- src/cli.rs
+++ src/cli.rs
@@ -5,9 +5,23 @@
 #[derive(clap::Parser)]
 #[command(version, about)]
 pub struct Cli {
+    /// Packet format to assume for the trace
+    #[arg(value_enum, short, long, default_value_t, global = true)]
+    pub format: PacketFormat,
+
     #[command(subcommand)]
     pub command: Command,
 }
 
 #[derive(clap::Subcommand)]
 pub enum Command {}
+
+/// Packet format
+#[derive(Copy, Clone, Default, Debug, PartialEq, Eq, clap::ValueEnum)]
+pub enum PacketFormat {
+    /// RISC-V Encapsulation structures
+    #[default]
+    Encap,
+    /// Siemens Messaging Infrastructure (SMI)
+    Smi,
+}

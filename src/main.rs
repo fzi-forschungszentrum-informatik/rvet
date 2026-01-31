203fd08b8632bdab5d26444623916699fc8b693e
Julian Ganz <ganz@fzi.de>
2026-01-31 18:05:54 +0100

    main: determine the target to use for raw binaries
    Some `riscv_etrace::binary::Binary` need to constructed for a predefined
    target. If it was specified by the user via the command line, we can use
    that. But otherwise, we need to infer it from parameters.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ mod reader;
 fn main() -> anyhow::Result<()> {
     use std::io::Write;
 
+    use riscv_etrace::instruction::info::MakeDecode;
+
     let args: cli::Cli = clap::Parser::parse();
 
     #[cfg(feature = "pager")]
@@ -27,6 +29,13 @@ fn main() -> anyhow::Result<()> {
         })
         .transpose()?
         .unwrap_or_default();
+
+    let target: riscv_isa::Target = args
+        .target
+        .map(Into::into)
+        .or_else(|| MakeDecode::infer_from_params(&params))
+        .unwrap_or_else(MakeDecode::rv32i_full);
+
     let decoder = packet::builder()
         .with_params(&params)
         .with_hart_index_width(args.hart_id_width)

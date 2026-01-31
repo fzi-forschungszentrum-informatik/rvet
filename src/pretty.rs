3e9dfb391208e5ce6bfb09d046575d954986bc99
Julian Ganz <ganz@fzi.de>
2026-01-31 22:05:16 +0100

    pretty: add type representing a single tracing item
    We want to display tracing items in a certain way, which partially
    depends on some settings that are supplied from the outside. This change
    introduces a `fmt::Display` type for handling that.

--- src/pretty.rs
+++ src/pretty.rs
@@ -1,3 +1,36 @@
 // Copyright (c) 2026 FZI Forschungszentrum Informatik
 // SPDX-License-Identifier: Apache-2.0
 //! Pretty printing and general rendering utilities
+
+use std::fmt;
+use std::num::NonZeroU8;
+
+use riscv_etrace::instruction::info::Info;
+use riscv_etrace::tracer::item::{self, Item};
+
+/// A single tracing item, rendered as a line
+pub struct ItemLine<I: Info> {
+    item: Item<I>,
+    address_width: NonZeroU8,
+    show_context: bool,
+}
+
+impl<I: Info + fmt::Display> fmt::Display for ItemLine<I> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let pc = self.item.pc();
+        let addr_width = self.address_width.get().into();
+
+        match self.item.kind() {
+            item::Kind::Regular(insn) => write!(f, "{pc:0addr_width$x}  {}", insn.info),
+            item::Kind::Trap(info) => write!(f, "{pc:0addr_width$x}  {info}"),
+            item::Kind::Context(ctx) => {
+                let privilege = ctx.privilege;
+                write!(f, "{0:addr_width$}  Context: {privilege}-mode", ' ')?;
+                if self.show_context {
+                    write!(f, " ctx: {}", ctx.context)?;
+                }
+                Ok(())
+            }
+        }
+    }
+}

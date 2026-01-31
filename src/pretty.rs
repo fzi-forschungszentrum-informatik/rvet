696fba5e1a0464e4486ebe4d4dfbc6f5eb82e98a
Julian Ganz <ganz@fzi.de>
2026-01-31 22:36:32 +0100

    pretty: add a generator for ItemLines
    We recently introduced `ItemLines` for displaying individual tracing
    items. However, we did not yet introduce any means for constructing
    those, and we want to skip some (context) items anyway.

--- src/pretty.rs
+++ src/pretty.rs
@@ -5,8 +5,48 @@
 use std::fmt;
 use std::num::NonZeroU8;
 
+use riscv_etrace::config::Parameters;
 use riscv_etrace::instruction::info::Info;
 use riscv_etrace::tracer::item::{self, Item};
+use riscv_etrace::types::Context;
+
+/// Generator for [`ItemLine`]s
+pub struct ItemGen {
+    context: Option<Context>,
+    address_width: NonZeroU8,
+    show_context: bool,
+}
+
+impl ItemGen {
+    /// Create a new [`ItemLine`] generator
+    ///
+    /// The generator will generate lines with some features (e.g. field widths)
+    /// tailored to the given [`Parameters`]
+    pub fn new(params: &Parameters) -> Self {
+        Self {
+            context: Default::default(),
+            address_width: params.iaddress_width_p.div_ceil(NonZeroU8::new(4).unwrap()),
+            show_context: !params.nocontext_p,
+        }
+    }
+
+    /// Process a single tracing [`Item`]
+    pub fn process_item<I: Info>(&mut self, item: Item<I>) -> Option<ItemLine<I>> {
+        if let item::Kind::Context(ctx) = item.kind() {
+            if Some(ctx) == self.context.as_ref() {
+                return None;
+            }
+
+            self.context = Some(*ctx);
+        }
+
+        Some(ItemLine {
+            item,
+            address_width: self.address_width,
+            show_context: self.show_context,
+        })
+    }
+}
 
 /// A single tracing item, rendered as a line
 pub struct ItemLine<I: Info> {

e7099f8b9ec0e8889878340dd81eaad092e5ab26
Julian Ganz <ganz@fzi.de>
2026-02-02 10:18:09 +0100

    binary: add type for riscv_etrace::binary::Binarys being produced
    The binary building infrastructure we plan to build will produce a
    specific `riscv_etrace::binary::Binary` type which we'll have to name in
    some fn signatures. Introducing a dedicated type safes us from spelling
    it out every time and makes it easier to switch out the instruction type
    in the future.

--- src/binary.rs
+++ src/binary.rs
@@ -1,3 +1,8 @@
 // Copyright (c) 2026 FZI Forschungszentrum Informatik
 // SPDX-License-Identifier: Apache-2.0
 //! Utilties for loading and assembling binaries
+
+use riscv_etrace::binary;
+
+/// Type of binary produced by the builder
+pub type Binary = binary::boxed::Binary<'static, riscv_isa::Instruction>;

98769b5208c66350092b4edc2a8c85bafa926347
Julian Ganz <ganz@fzi.de>
2026-02-02 19:52:54 +0100

    stat: add types for encap packet headers
    We plan to introduce a "stat" command that shows a summary of the
    packets in a trace file, i.e. it will count packets belonging to
    different groups. In the case of RISC-V encap packets these groups
    include null packets and normal encapsulation structures originating
    from different sources.
    
    This change introduces types representing these groups and extracting a
    group from a packet.

--- src/stat.rs
+++ src/stat.rs
@@ -1,3 +1,47 @@
 // Copyright (c) 2026 FZI Forschungszentrum Informatik
 // SPDX-License-Identifier: Apache-2.0
 //! Utilities for gathering of "statistics"
+
+use riscv_etrace::packet::encap;
+
+/// Header fields of a RISC-V encapsulation packet
+#[derive(Copy, Clone, Debug)]
+pub enum EncapHeader {
+    Null(EncapNullHeader),
+    Normal(EncapNormalHeader),
+}
+
+impl<P> From<encap::Packet<P>> for EncapHeader {
+    fn from(packet: encap::Packet<P>) -> Self {
+        match packet {
+            encap::Packet::NullIdle { flow } => Self::Null(EncapNullHeader { flow, align: false }),
+            encap::Packet::NullAlign { flow } => Self::Null(EncapNullHeader { flow, align: true }),
+            encap::Packet::Normal(n) => Self::Normal(n.into()),
+        }
+    }
+}
+
+/// Header fields of a RISC-V null encapsulation structure
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct EncapNullHeader {
+    pub flow: u8,
+    pub align: bool,
+}
+
+/// Header fields of a RISC-V normal encapsulation structure
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct EncapNormalHeader {
+    pub flow: u8,
+    pub src_id: u16,
+    pub timestamp: bool,
+}
+
+impl<P> From<encap::Normal<P>> for EncapNormalHeader {
+    fn from(packet: encap::Normal<P>) -> Self {
+        Self {
+            flow: packet.flow(),
+            src_id: packet.src_id(),
+            timestamp: packet.timestamp().is_some(),
+        }
+    }
+}

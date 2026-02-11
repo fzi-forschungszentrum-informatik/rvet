ee83f4cd2ce04d0ca60ca785f5216d28d00356d1
Julian Ganz <ganz@fzi.de>
2026-02-11 11:45:40 +0100

    csv: add type representing CSV fields
    We intend to instruce a command for transforming a trace into a CSVs.
    We want the CSV fields to be configurable. Still, the sets of fields
    users may choose from is predefined due to obvious reasons. This change
    introduces the set in the form of an enum we may use for configuration.

--- src/csv.rs
+++ src/csv.rs
@@ -1,3 +1,43 @@
 // Copyright (c) 2026 FZI Forschungszentrum Informatik
 // SPDX-License-Identifier: Apache-2.0
 //! CSV export utilities
+
+/// CSV field to include
+#[derive(Copy, Clone, Debug, clap::ValueEnum)]
+pub enum Field {
+    /// Source or HART id
+    Source,
+    /// PC or EPC
+    Address,
+    /// Instruction
+    Instruction,
+    /// Privilege
+    Privilege,
+    /// Debug/trace context
+    Context,
+    /// Exception flag (also set for interrupts)
+    Exception,
+    /// Interrupt flag
+    Interrupt,
+    /// Exception/interrupt cause
+    ECause,
+    /// Value of the privilege specific `tval` register
+    TVal,
+}
+
+impl Field {
+    /// Retrieve the header to use for this field
+    pub fn header(self) -> &'static str {
+        match self {
+            Self::Source => "SOURCE",
+            Self::Address => "ADDRESS",
+            Self::Instruction => "INSTRUCTION",
+            Self::Privilege => "PRIVILEGE",
+            Self::Context => "CONTEXT",
+            Self::Exception => "EXCEPTION",
+            Self::Interrupt => "INTERRUPT",
+            Self::ECause => "ECAUSE",
+            Self::TVal => "TVAL",
+        }
+    }
+}

652f5f84d57ad895a992ca029e3c251bc3ca6aeb
Julian Ganz <ganz@fzi.de>
2026-02-20 13:10:30 +0100

    pretty: remove now unused ItemLine
    The type was used when `Printer` did not write out items itself but made
    this the caller's responsibility.

--- src/pretty.rs
+++ src/pretty.rs
@@ -72,30 +72,3 @@ impl<W: Write> Printer<W> {
         Ok(())
     }
 }
-
-/// A single tracing item, rendered as a line
-pub struct ItemLine<I: Info> {
-    item: Item<I>,
-    address_width: NonZeroU8,
-    show_context: bool,
-}
-
-impl<I: Info + fmt::Display> fmt::Display for ItemLine<I> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let pc = self.item.pc();
-        let addr_width = self.address_width.get().into();
-
-        match self.item.kind() {
-            item::Kind::Regular(insn) => write!(f, "{pc:0addr_width$x}  {}", insn.info),
-            item::Kind::Trap(info) => write!(f, "{pc:0addr_width$x}  {info}"),
-            item::Kind::Context(ctx) => {
-                let privilege = ctx.privilege;
-                write!(f, "{0:addr_width$}  Context: {privilege}-mode", ' ')?;
-                if self.show_context {
-                    write!(f, " ctx: {}", ctx.context)?;
-                }
-                Ok(())
-            }
-        }
-    }
-}

a3479b2c7c0e6b8fe824c5a4aa3be1bbd4337fbb
Julian Ganz <ganz@fzi.de>
2026-02-23 16:58:41 +0100

    pretty: include instruction bits in output

--- src/pretty.rs
+++ src/pretty.rs
@@ -49,7 +49,10 @@ impl<W: Write> Printer<W> {
         }
 
         match item.kind() {
-            item::Kind::Regular(insn) => writeln!(self.out, "{pc:addr_width$x}  {}", insn.info.0),
+            item::Kind::Regular(insn) => {
+                let (insn, bits) = &insn.info;
+                writeln!(self.out, "{pc:addr_width$x}  {:<8}  {insn}", bits.to_string())
+            }
             item::Kind::Trap(info) => writeln!(self.out, "{pc:addr_width$x}  {info}"),
             item::Kind::Context(ctx) if self.context.as_ref() != Some(ctx) => {
                 self.context = Some(*ctx);

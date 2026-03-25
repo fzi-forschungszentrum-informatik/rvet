eb47804109e82223b26e33327fec8fa5c74a80ee
Julian Ganz <ganz@fzi.de>
2026-03-25 16:17:09 +0100

    profile: add type for single call tree's profile
    In order to build up a profile we'll need data structures suitable for
    holding (partially processed) profiling data. This change introduces
    such data types.

--- src/profile.rs
+++ src/profile.rs
@@ -2,6 +2,72 @@
 // SPDX-License-Identifier: Apache-2.0
 //! Profile creation and handling utilities
 
+use std::collections::BTreeMap;
+use std::sync::Arc;
+
+use crate::stack::Frame;
+
+/// Profile
+///
+/// Profiling data for a single call tree.
+#[derive(Clone, Default, Debug)]
+pub struct Profile {
+    inner: BTreeMap<Arc<Frame>, Metrics>,
+}
+
+impl Profile {
+    /// Retrieve the profiling data as a mapping of stacks to [`Metrics`]
+    pub fn as_map(&self) -> &BTreeMap<Arc<Frame>, Metrics> {
+        &self.inner
+    }
+
+    /// Retrieve the profiling data as a mut mapping of stacks to [`Metrics`]
+    pub fn as_map_mut(&mut self) -> &mut BTreeMap<Arc<Frame>, Metrics> {
+        &mut self.inner
+    }
+
+    /// Convert into a mapping of stacks to [`Metrics`]
+    pub fn into_map(self) -> BTreeMap<Arc<Frame>, Metrics> {
+        self.inner
+    }
+}
+
+/// Metrics for a single stack [`Frame`]
+#[derive(Clone, Default, Debug)]
+pub struct Metrics {
+    map: BTreeMap<u64, Cost>,
+}
+
+impl Metrics {
+    /// Increase the [`Cost`] associated with a PC by single tick
+    pub fn tick(&mut self, pc: u64) {
+        self.map.entry(pc).or_default().tick();
+    }
+
+    /// Merge these metrics with a different one
+    pub fn merge(&mut self, other: &Self) {
+        self.merge_iter(other.map.iter().map(|(k, v)| (*k, *v)))
+    }
+
+    /// Add costs provided via an [`Iterator`]
+    pub fn merge_iter(&mut self, map: impl IntoIterator<Item = (u64, Cost)>) {
+        map.into_iter().for_each(|(k, v)| {
+            let value = self.as_map_mut().entry(k).or_default();
+            *value += v;
+        });
+    }
+
+    /// Retrieve a mapping from PCs to [`Cost`]s
+    pub fn as_map(&self) -> &BTreeMap<u64, Cost> {
+        &self.map
+    }
+
+    /// Retrieve a mutable mapping from PCs to [`Cost`]s
+    pub fn as_map_mut(&mut self) -> &mut BTreeMap<u64, Cost> {
+        &mut self.map
+    }
+}
+
 /// Cost associated with a profiled item (e.g. an instruction)
 #[derive(Copy, Clone, Default, Debug)]
 pub struct Cost {
